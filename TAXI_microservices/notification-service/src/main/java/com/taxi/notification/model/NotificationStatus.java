a7763ad6c6c77822c1a3d4a697c7f7b253fdd85d
hanlaev.m5 <168988406+HanlaevMuhammed@users.noreply.github.com>
2026-05-11 00:27:34 +0700

    Revert "Future/notification service"

--- TAXI_microservices/notification-service/src/main/java/com/taxi/notification/model/NotificationStatus.java
+++
@@ -1,8 +0,0 @@
-package com.taxi.notification.model;
-
-public enum NotificationStatus {
-    PENDING,
-    PROCESSING,
-    SENT,
-    FAILED
-}
--- TAXI_microservices/trip-service/src/main/java/com/taxi/trip/model/TripStatus.java
+++
@@ -1,9 +0,0 @@
-package com.taxi.trip.model;
-
-public enum TripStatus {
-    PENDING,
-    ACCEPTED,
-    IN_PROGRESS,
-    COMPLETED,
-    CANCELLED
-}
